4aee5c884530b54b51df9434ebdb59a8a593bed9
Priyanshu Mathur <mathurpriyanshu2006@gmail.com>
2026-02-14 11:36:57 +0530

    resolved port issue

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from rag_pipeline import RAGPipeline
-
+import os 
 app = Flask(__name__)
 CORS(app)
 
@@ -33,4 +33,5 @@ def ask():
     return jsonify({"answer": answer})
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
